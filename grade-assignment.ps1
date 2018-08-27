efd6020348862bf06cd0f7b12abfc1bbcda9451f
jallen <jallen@snow.edu>
2018-08-27 15:39:32 -0600

    Add script to help grade CS4220 assignments

---
+++ grade-assignment.ps1
@@ -0,0 +1,29 @@
+﻿# grade-assignment
+
+param(
+    [string]$commitUrl,
+    [string]$student
+)
+
+if($commitUrl.Contains("/commit")){
+    $indexOfCommit = $commitUrl.IndexOf("/commit");
+    $repo = $commitUrl.Substring(0,$indexOfCommit);
+    $hash = $commitUrl.Substring($indexOfCommit+8);
+}
+else { # just a repo url
+    $repo = $commitUrl
+    $hash = $null
+}
+write-host "Taking $repo @ $hash for $student"
+
+set-location c:\git
+if(test-path $student -PathType Container){
+    remove-item $student -Recurse -Force
+}
+git clone $repo $student
+set-location $student
+if($hash -ne $null){
+    git checkout $hash
+}
+
+gci *.sln -Recurse | select-object -First 1 | %{Start-Process $_}

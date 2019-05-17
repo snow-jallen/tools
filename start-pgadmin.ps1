4e68937400da0cfaab5ec957af02a56b5087cd4a
Jonathan Allen <jallen@snow.edu>
2019-05-17 10:56:22 -0600

    Add new start-pgadmin script

---
+++ start-pgadmin.ps1
@@ -0,0 +1,9 @@
+param(
+    [string]$containerName="pgadmin",
+    [int]$localPort=5005,
+    [string]$loginEmail="admin@snow.edu",
+    [string]$loginPassword='P@ssword1'
+)
+docker run -d  --name $containerName -p "$($localPort):80" -e PGADMIN_DEFAULT_EMAIL=$loginEmail -e PGADMIN_DEFAULT_PASSWORD=$loginPassword dpage/pgadmin4
+
+start-process "http://localhost:$localPort"

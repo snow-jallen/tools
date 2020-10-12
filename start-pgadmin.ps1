8857eb03fe57fc5f14db003a8dde8e653e39f58f
J Allen <42281341+snow-jallen@users.noreply.github.com>
2020-10-12 11:55:13 -0600

    Update start-pgadmin.ps1
    Wait a bit before hitting the url

--- start-pgadmin.ps1
+++ start-pgadmin.ps1
@@ -5,5 +5,6 @@ param(
     [string]$loginPassword='P@ssword1'
 )
 docker run -d  --name $containerName -p "$($localPort):80" -e PGADMIN_DEFAULT_EMAIL=$loginEmail -e PGADMIN_DEFAULT_PASSWORD=$loginPassword dpage/pgadmin4
-
+write-host "Starting up pgadmin..."
+start-sleep -seconds 5
 start-process "http://localhost:$localPort"
